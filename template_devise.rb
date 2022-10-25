d6d96d85c08f7d9f3ad3608cd20770649d485c24
Raphaël Jubé <91531495+RafJube@users.noreply.github.com>
2022-10-25 11:29:07 +0200

    Correction for hello_controller

--- template_devise.rb
+++ template_devise.rb
@@ -203,7 +203,7 @@ after_bundle do
     // This example controller works with specially annotated HTML like:
     //
     // <div data-controller="hello">
-    //   <h1 data-target="hello.output"></h1>
+    //   <h1 data-hello-target="output"></h1>
     // </div>
 
     import { Controller } from "stimulus"
